90a4d976b9d82f821c6e825630b8d7993cbe5105
engineer <newgalaxy401@gmail.com>
2024-11-02 10:31:23 +0300

    make base structure

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Header from './templates/Header'
+//import Main from './templates/Main'
+//import Footer from './templates/Footer'
+//import Sidebar from './templates/Sidebar'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='header'>
+      <Header/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
